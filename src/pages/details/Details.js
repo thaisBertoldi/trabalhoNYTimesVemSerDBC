6bb607070921237c89883b652ee7e12ec9775659
Thais B <thais_brt@hotmail.com>
2022-04-02 11:20:18 -0300

    Page details with content

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import moment from "moment";
 import { DetailsContext } from "../../contexts/DetailsContext";
 
 const Details = () => {
@@ -6,7 +7,33 @@ const Details = () => {
 
   return (
     <div className="container">
-      <h1>{detailArticle.title}</h1>
+      <div>
+        {detailArticle.multimedia === null ? (
+          <img
+            src="https://cna.com.br/Content/uploads/blogposts/os-melhores-sites-de-noticias-em-ingles-para-estudar.jpg"
+            width="650px"
+            alt="alt"
+          />
+        ) : (
+          <img src={detailArticle.multimedia[0].url} width="800px" alt="" />
+        )}
+      </div>
+      <div>
+        <h1>{detailArticle.title}</h1>
+        <small>
+          {moment(detailArticle.published_date).startOf("day").fromNow()}
+        </small>
+        <p>{detailArticle.byline}</p>
+        <p>{detailArticle.abstract}</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Reprehenderit est ex unde, molestias officia ducimus error ipsa
+          veritatis similique, voluptatem velit ut repudiandae aliquid, quas id
+          nam quisquam consequatur rem provident nemo architecto eveniet
+          voluptatibus! Voluptas, molestiae. Labore dolorum blanditiis, nisi
+          expedita rerum temporibus officia non cumque pariatur sint. Porro?
+        </p>
+      </div>
     </div>
   );
 };
